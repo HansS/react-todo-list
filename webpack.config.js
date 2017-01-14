a8fd083c1838b8ad770ab2a8f72043201559f21e
Hans Schenker <Hans Schenker>
2017-01-14 08:12:03 +0100

    change webpack.config.js hmr preset

--- webpack.config.js
+++ webpack.config.js
@@ -23,7 +23,7 @@ module.exports = {
         loader: 'babel-loader',
         exclude: /node_modules/,
         query: {
-          presets: ['react', 'es2015', 'react-hmre']
+          presets: ['react', 'es2015', require.resolve('babel-preset-react-hmre')]
         }
       }
     ]
